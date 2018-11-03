9646b8cb8de28b14543eaee6bda4bd940538c504
Garrett Smith <19smitgr@users.noreply.github.com>
2018-11-03 14:13:08 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var canvas = document.getElementById('game');
+var ctx = canvas.getContext('2d');
+
+// make canvas full screen
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// your code here:
+
